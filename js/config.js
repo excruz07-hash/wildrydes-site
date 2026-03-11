2d60cffae2ec8c773eebadfcfb15ee03e534f451
excruz07-hash <excruz07@gmail.com>
2026-03-10 20:16:04 -0500

    Update config.js to remove sensitive data
    Removed userPoolClientSecret from configuration.

--- js/config.js
+++ js/config.js
@@ -2,13 +2,13 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_Y067gwwQV', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '10grqnuobgdgvvnbkdtf75mst1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        userPoolClientSecrect: '38m16tv9cnkc5d4d31mk6s6f0bich65e0ha7kpja2ej9o1to9ns'
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
