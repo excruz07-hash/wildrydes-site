9a1c00dcc29df40aa57a475b26d7e295836a2737
excruz07-hash <excruz07@gmail.com>
2026-03-10 20:25:42 -0500

    Modify userPoolClientId in config.js
    Updated userPoolClientId in the configuration.

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_Y067gwwQV', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '10grqnuobgdgvvnbkdtf75mst1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '77dekdic1njvvqqdl5od6946g8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
